8bf1999ccbf2eb0f96b8fbaa021c487a25e05d15
PhatVang <hungphat123412@gmail.com>
2026-03-16 17:26:52 +0700

    khi bấm vào mua hàng sẽ hiện lên bảng thống báo phải đăng nhập

--- BTTH(buoi3)/muahang.php
+++ BTTH(buoi3)/muahang.php
@@ -42,6 +42,7 @@ if (!$conn) {
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="hehe.css">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
 <body>
 
